6ef469691a0818be7bf56c6992785e7fc973e776
Trivedi Shivansh <shivanahtrivedi@gmail.com>
2025-02-05 19:37:46 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.print("hello world with java\n this is my first \n");
+        System.out.println("hello world with java");
+    }
+}
+
---
+++ variable.java
@@ -0,0 +1,26 @@
+public class variable {
+
+    public static void main(String[] args) {
+        int a = 39;
+        int b = 40;
+        int sum = a+b;
+        System.out.println(sum);
+
+        int diff = a-b;
+        System.out.println(diff);
+
+        int multi = a*b;
+        System.out.println(multi);
+
+        int divide = a%b;
+        System.out.println(divide);
+
+        int c = 10;
+        int g = 5;
+
+        System.out.println(c*g/c-g);
+        
+        
+
+    }
+}
